877cd2a68f7f31b08548899632a9abcb8bd368e6
sytallax <sytallax@gmail.com>
2024-03-28 13:17:37 -0500

    feat: add gitsigns plugin

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,19 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+        require("gitsigns").setup({
+            current_line_blame = true,
+            current_line_blame_opts = {
+                virt_text = true,
+                delay = 1000 / 3,
+            },
+            signs = {
+                add = { text = "+" },
+                change = { text = "~" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
+            },
+        })
+    end,
+}
